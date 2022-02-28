56aeebacb7e1804424fb12b240fb324a0b628f94
holypar <rapsingh@ucdavis.edu>
2022-02-27 20:28:09 -0800

    phase 3

--- libfs/fs.c
+++ libfs/fs.c
@@ -44,6 +44,7 @@ struct __attribute__((packed)) fdEntry {
 };
 
 struct __attribute__((packed)) fdTable {
+	int openFiles;
 	struct fdEntry fdEntries[FS_OPEN_MAX_COUNT];	
 };
 
@@ -53,28 +54,35 @@ struct rootDirEntry root_dir[MAX_ROOT_FILES];
 uint16_t *fatTable;
 struct fdTable fdTable;
 
+//Global Variable for if Virtual Disk is opened
+int opened_vd = -1;
+
 
 int fs_mount(const char *diskname)
 {
-	// 1.) Open Disk + Error Handling
+    // Open Disk + Error Handling
 	int open_disk, read_disk, disk_size; // fat_block;
+
 	open_disk = block_disk_open(diskname);
 	if (open_disk == -1){
 		//printf("Disk cannot open"); // Error checking for when testing
 		return -1;
 	}
 
-	// 2.) Load Meta-info
-	// reads in opened disk and reads block 0 -> store in superblock struct
+	// Reads in opened disk and reads block 0 -> store in superblock struct
 	read_disk = block_read(0, &super_block);
 	if (read_disk == -1){
 		return -1;
 	}	
 
-	// do we compare vs ECS150-FS?
-	if (memcmp("ECS150FS",super_block.signature, sizeof(super_block.signature)) != 0){ 
-		return -1; 
+	// Error Handling to check if signature matches
+	if (memcmp("ECS150FS", super_block.signature, sizeof(super_block.signature)) != 0){ 
+		return -1;  
 	}
+
+	// If Virtual Disk was opened successfully, switch from -1 to 0.
+	opened_vd = 0;
+
 
 	// Obtain disk size + Error Handling
 	disk_size = block_disk_count();
@@ -94,17 +102,17 @@ int fs_mount(const char *diskname)
 		}
 	}
 
-	//the first entry of the fat table has to be FAT_EOC
+	// The first entry of the fat table has to be FAT_EOC (Error Handling)
 	if (fatTable[0] != FAT_EOC){
 		return -1;
 	}
 
-// read into the root directory + Error Handling if fails
+	// Read into the root directory + Error Handling 
 	if (block_read(super_block.rootBlockIndex, &root_dir) == -1) {
 		return -1;
 	}
 
-
+	// Checking if fd entries match 0
 	for (int i = 0; i < FS_OPEN_MAX_COUNT; i++){
 		fdTable.fdEntries[i].status = 0; 
 	}
@@ -116,31 +124,32 @@ int fs_umount(void)
 {
 	int close_disk, write_block, write_root;
 
-	// writing out of disk 
+	// If FS not mounted to write + Error Handling 
 	write_block = block_write(0, &super_block);
 	if (write_block == -1){
 		return -1;
 	}
 
-	// assuming you have to write out to root dir also???
+	// Writing to root index + Error Handling
 	write_root = block_write(super_block.rootBlockIndex, &root_dir);
 	if (write_root == -1){
 		return -1;
 	}
 
-	// fat writing 
+	// Fat table writing + Error Handling
 	for(int i = 1; i < super_block.fatBlocks; i++){
 		if (block_write(i, fatTable + (i-1)*BLOCK_SIZE) == -1){
 			return -1;
-		};
+		}
 	}
  
+	// Close disk + Error Handling
 	close_disk = block_disk_close();
 	if(close_disk == -1){
 		return -1;
 	}
 
-	// deallocate memory
+	// Deallocate memory
 	free(fatTable);
 
 	return 0;
@@ -149,8 +158,9 @@ int fs_umount(void)
 int fs_info(void)
 {
 
-	/* TODO: Phase 1 Part 2*/
-	// if file was not opened return -1;
+	if (opened_vd == -1){
+		return -1;
+	}
 
 	// Root Ratio Entry
 	int freeRootEntry = 0;
@@ -175,6 +185,7 @@ int fs_info(void)
 	printf("data_blk_count=%d\n", super_block.amountDataBlocks);
 	printf("fat_free_ratio=%d/%d\n",freeFatEntry, super_block.amountDataBlocks);
 	printf("rdir_free_ratio=%d/%d\n",freeRootEntry, MAX_ROOT_FILES);
+
 	return 0;
 }
 
@@ -182,13 +193,28 @@ int fs_create(const char *filename)
 {
 
 	// Error Handling
-	if(filename == NULL){
+
+	if(filename == NULL || opened_vd == -1 || strlen(filename) > FS_FILENAME_LEN){
 		return -1;
+	}
+
+	int file_exists = 0;
+	// Error Handling loop to check if file already exists in directory
+	for (int i = 0; i < MAX_ROOT_FILES; i++){
+		if(root_dir[i].fileName[0] != '\0'){
+			file_exists++;
+		}
+		if(strcmp(root_dir[i].fileName, filename) == 0){
+			return -1; // Means file already exists in Root Directory
+		}
+		// Error Handling if Directory has reached its max files
+		if (file_exists == FS_FILE_MAX_COUNT){
+			return -1; 
+		}
 	}
 
 	for (int i = 0; i < MAX_ROOT_FILES; i++){
 		if(root_dir[i].fileName[0] == '\0'){ 
-			//char* thefile = filename;
 			memcpy(root_dir[i].fileName,filename, FS_FILENAME_LEN);
 			root_dir[i].indexOfFirstDataBlock = FAT_EOC; // First index set to FAT_EOC
 			root_dir[i].fileSize = 0; // Setting size to 0
@@ -196,10 +222,7 @@ int fs_create(const char *filename)
 		}
 		
 	}
-        /* TODO: Phase 2 */
-		// from fs.h
-			// "String @filename must be NULL-terminated and its total
- 			//* length cannot exceed %FS_FILENAME_LEN characters"
+
 	return 0;
 }
 
@@ -213,14 +236,9 @@ int fs_delete(const char *filename)
 	*/
         /* TODO: Phase 2 */
 	// Error Handling
-	if(filename == NULL){
+	if(filename == NULL || opened_vd == -1 || strlen(filename) > FS_FILENAME_LEN){
 		return -1;
 	}
-
-		// check if Fat Block is == FAT_EOC
-		/* Removing a file is the opposite procedure: the file’s 
-		entry must be emptied and all the data blocks containing the file’s 
-		contents must be freed in the FAT.*/
 
 		uint16_t fat_position;
 		// Empty for loop
@@ -254,7 +272,11 @@ int fs_delete(const char *filename)
 
 int fs_ls(void)
 {
-	// Printing LS - Phase 2
+	// Error Handling if Disk is not opened
+	if (opened_vd == -1){
+		return -1;
+	}
+
 	printf("FS Ls:\n");
 	for (int i=0; i < MAX_FILE_NAME_SIZE; i++){
 		if( root_dir[i].fileName[0] != '\0'){
@@ -280,7 +302,11 @@ int fs_open(const char *filename)
 {
 	// Error Handling
 	// 32 files can't be opened ; FS_OPEN_MAX_COUNT 
-	if (filename == NULL){
+	if (filename == NULL || opened_vd == -1 || strlen(filename) > FS_FILENAME_LEN){
+		return -1;
+	}
+
+	if (fdTable.openFiles > FS_OPEN_MAX_COUNT){
 		return -1;
 	}
 
@@ -295,9 +321,10 @@ int fs_open(const char *filename)
 		if(strcmp((char*)root_dir[i].fileName, filename) == 0){      // check if filename even exists in the root dir beforehand
 			for(int i = 0; i < FS_OPEN_MAX_COUNT; i++){ //Max Count = 16
 				if((fdTable.fdEntries[i].status == 0)){		
-					memcpy(fdTable.fdEntries[i].fileName, filename, FS_FILENAME_LEN); // FS_FILENAME_LEN         check for first FDentry that's status is 0 
+					memcpy(fdTable.fdEntries[i].fileName, filename, FS_FILENAME_LEN); // FS_FILENAME_LEN = 16         check for first FDentry that's status is 0 
 					fdTable.fdEntries[i].offset = 0;
 					fdTable.fdEntries[i].status = 1;
+					fdTable.openFiles += 1;
 					returnFD = i;
 
 					// files_open++; // counter for opened files
@@ -312,21 +339,26 @@ int fs_open(const char *filename)
 
 int fs_close(int fd)
 {
-        fdTable.fdEntries[fd].status = 0;
-		fdTable.fdEntries[fd].offset = 0;
+	// Error Handling if Out of Bounds 0 < x < 32 and if file is not opened
+	if (fd > FS_OPEN_MAX_COUNT || fd < 0 || fdTable.fdEntries[fd].fileName[0] == '\0'){
+		return -1;
+	}
 
-		// file decrease counter ; files_open--;
-		return 0;
+    fdTable.fdEntries[fd].status = 0;
+	fdTable.fdEntries[fd].offset = 0;
+	fdTable.openFiles -= 1;
+	return 0;
 }
 
 int fs_stat(int fd)
 {
+
+	if (fd > FS_OPEN_MAX_COUNT || fd < 0 || fdTable.fdEntries[fd].fileName[0] == '\0'){
+		return -1;
+	}
+
 	int returnFileSize = -1;
  	
-	// Error Handling
-	//if (fdTable.fdEntries[fd].fileName[0] == '\0'){
-		//return -1; 
-	//}
 
 	for (int i = 0; i < MAX_ROOT_FILES; i++){
 		//once a file in the root directory matches the fdentry file name, we return that rootentry filesize
@@ -340,7 +372,16 @@ int fs_stat(int fd)
 
 int fs_lseek(int fd, size_t offset)
 {
-	// Need error handling here
+
+	if (fd > FS_OPEN_MAX_COUNT || fd < 0 || fdTable.fdEntries[fd].fileName[0] == '\0'){
+		return -1;
+	}
+
+	//if someone is trying to go somewhere which extends off the size of the actual file. 
+	if (offset > fs_stat(fd)){      
+		return -1;
+	}
+
     fdTable.fdEntries[fd].offset = offset;
 	return 0;
 }
